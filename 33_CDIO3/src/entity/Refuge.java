06f6340dbd225c95a3b1b5f6f9bbecb09213a485
Mikkel <Mikkel@192.168.0.104>
2016-11-10 22:14:14 +0100

    Added comments to the refuge field

--- 33_CDIO3/src/entity/Refuge.java
+++ 33_CDIO3/src/entity/Refuge.java
@@ -1,19 +1,28 @@
 package entity;
 
-
-public class Refuge extends Field {
-
+public class Refuge extends Field 
+{
+	//Instance variables
 	private int bonus;
 
+	/**
+	 * Object refuge constructor
+	 * @param fieldName The name of the refuge field
+	 * @param bonus The bonus that the refuge field gives.
+	 */
 	public Refuge(String fieldName, int bonus)
 	{
 		super(fieldName);
 		this.bonus = bonus;
 	}
 	
+	/**
+	 * Method landOnField decides what to be done when player lands on a refuge Field.
+	 * @param player The player to land on the field.
+	 */
 	public void landOnField(Player player)
 	{
-		player.changeAccountBalance(bonus);
+		player.changeAccountBalance(bonus); //Gives the player a bonus to his balance.
 	}
 }
 
