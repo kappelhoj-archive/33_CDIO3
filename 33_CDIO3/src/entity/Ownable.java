4623c499ab94c87a069e012462a74673831fc3cf
Mikkel <Mikkel@192.168.0.104>
2016-11-10 21:38:06 +0100

    Added comments to the ownable class.

--- 33_CDIO3/src/entity/Ownable.java
+++ 33_CDIO3/src/entity/Ownable.java
@@ -2,9 +2,15 @@ package entity;
 
 public abstract class Ownable extends Field
 {
+	//Instance variables
 	private int price;
 	private Player owner;
 	
+	/**
+	 * Object Ownable constructor
+	 * @param fieldName The name of the field.
+	 * @param price The price of the Ownable field.
+	 */
 	public Ownable(String fieldName, int price) 
 	{
 		super(fieldName);
@@ -12,19 +18,32 @@ public abstract class Ownable extends Field
 		this.owner = null;
 	}
 
+	/**
+	 * The method getRent returns the rent to be paid by the player who lands on the Ownable field.
+	 * A method to be overridden by subclasses.
+	 * @return Returns the rent to be paid.
+	 */
 	public int getRent()
 	{
 		return 0;
 	}
 	
+	/**
+	 * Method getOwner returns the owner of the field.
+	 * @return Returns the player who owns the field.
+	 */
 	public Player getOwner()
 	{
 		return owner;
 	}
 	
+	/**
+	 * Method isFieldOwned check if the field is owned by a player.
+	 * @return Returns true if the field is owned.
+	 */
 	public boolean isFieldOwned()
 	{
-		if (owner == null)
+		if (owner == null) //Checks if the field has a owner.
 		{
 			return false;
 		}
@@ -34,12 +53,18 @@ public abstract class Ownable extends Field
 		}
 	}
 	
+	/**
+	 * Method buyField Lets the player buy a Ownable field.
+	 * @param player The player to buy the field.
+	 * @return Returns true if the buy succeeded.
+	 */
 	public boolean buyField(Player player)
 	{
-		if (player.getAccountBalance() > price)
+		if (player.getAccountBalance() > price) //Checks if the player has enough money to buy the field.
 		{
-			player.changeAccountBalance(-price);
-			owner = player;
+			player.changeAccountBalance(-price); //Subtracts the price of the field from the player account balance.
+			owner = player;                      //Sets the player to be the owner of the field.
+			player.changePlayerFortune(price);   //Adds the price of the field to the fortune of the player.
 			return true;
 		}
 		else
@@ -47,12 +72,16 @@ public abstract class Ownable extends Field
 			return false;
 		}
 	}
-	
+		
+	/**
+	 * Method landOnField decides what to be done when player lands on a Ownable Field.
+	 * @param player The player to land on the field.
+	 */
 	public void landOnField(Player player)
 	{
-		if (!getOwner().getPlayerName().equals(player.getPlayerName()))
+		if (!getOwner().getPlayerName().equals(player.getPlayerName())) //Checks if the owner of the field and the player who landed on the field isn't the same
 		{
-			player.payRent(getOwner(), this.getRent());
+			player.payRent(getOwner(), this.getRent()); //player pays the owner of the field.
 		}
 	}
 }
