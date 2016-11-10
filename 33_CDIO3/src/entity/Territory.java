e64580dbe7f92d2c45dcbbefd0e217ead32182fc
Jeppe <keke3k2@gmail.com>
2016-11-10 10:49:48 +0100

    Updated Territory class

--- 33_CDIO3/src/entity/Territory.java
+++ 33_CDIO3/src/entity/Territory.java
@@ -2,4 +2,17 @@ package entity;
 
 public class Territory extends Ownable {
 	private int rent;
+	
+	/**
+	 * Territory constructor
+	 * @param name
+	 * @param price
+	 * @param owner
+	 * @param rent
+	 */
+	public Territory(String name, int price, Player owner, int rent)
+	{
+		super(name, price, owner);
+		this.rent = rent;
+	}
 }
