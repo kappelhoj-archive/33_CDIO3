00efb384328e9511d5c352ae7459c96793a3f1c6
Mikkel <Mikkel@192.168.0.104>
2016-11-10 22:07:38 +0100

    Updated Territory with comments.

--- 33_CDIO3/src/entity/Territory.java
+++ 33_CDIO3/src/entity/Territory.java
@@ -2,14 +2,25 @@ package entity;
 
 public class Territory extends Ownable 
 {
+	//Instance variables
 	private int rent;
 	
+	/**
+	 * Object Territory constructor
+	 * @param fieldName The name of the field
+	 * @param price The price of the field
+	 * @param rent The rent it costs to land on the field if the field is owned.
+	 */
 	public Territory(String fieldName, int price, int rent)
 	{
 		super(fieldName, price);
 		this.rent = rent;
 	}
 	
+	/**
+	 * The method getRent returns the rent to be paid by the player who lands on the territory field.
+	 * @return Returns the rent to be paid.
+	 */
 	public int getRent()
 	{
 		return rent;
