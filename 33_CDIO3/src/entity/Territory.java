9fde70e934112031f60c4fd8bd647429bfd01a17
Mikkel <Mikkel@10.16.230.240>
2016-11-10 10:52:02 +0100

    Territory updated

--- 33_CDIO3/src/entity/Territory.java
+++ 33_CDIO3/src/entity/Territory.java
@@ -1,11 +1,7 @@
 package entity;
 
-<<<<<<< HEAD
-public class Territory extends Ownerable 
+public class Territory extends Ownable 
 {
-=======
-public class Territory extends Ownable {
->>>>>>> branch 'implement' of https://github.com/ArvidLangsoe/33_CDIO3.git
 	private int rent;
 	
 	/**
