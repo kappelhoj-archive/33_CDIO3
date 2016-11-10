a49b12811e47e1e1fc45681b097d2ea4fd6a4151
Jeppe <keke3k2@gmail.com>
2016-11-10 11:51:25 +0100

    Updated LaborCamp

--- 33_CDIO3/src/entity/LaborCamp.java
+++ 33_CDIO3/src/entity/LaborCamp.java
@@ -2,5 +2,24 @@ package entity;
 
 public class LaborCamp extends Ownable
 {
+	private int baseRent;
+	
+	public LaborCamp(String fieldName, int price, Player owner) {
+		super(fieldName, price, owner);
+		this.baseRent = 100;
+	}
+
+	public int getRent()
+	{
+		int amountOfLaborCamps = super.getOwner().getAmountOfLaborCamps();
+		int rent;
+		//diceSum skal ændres
+		int diceSum = 0;
+		
+		rent = baseRent * amountOfLaborCamps * diceSum;
+		
+		return rent;
+	}
+	
 	
 }
