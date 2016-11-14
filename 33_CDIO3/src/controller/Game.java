34ee2d6c576396224ba0b4aa26a491bcfd3efa1d
Arvid Langsø <arvid.k.langsoe@live.dk>
2016-11-14 16:40:07 +0100

    Changed some formatting of the code.

--- 33_CDIO3/src/controller/Game.java
+++ 33_CDIO3/src/controller/Game.java
@@ -5,7 +5,6 @@ import boundary.TUI;
 import entity.DiceCup;
 import entity.Player;
 
-
 public class Game {
 	int turn;
 	Player[] players;
@@ -14,52 +13,51 @@ public class Game {
 	DiceCup dice;
 
 	public Game() {
-		//Creates a DiceCup
-		dice=new DiceCup();
-		//Start the TUI
+		// Creates a DiceCup
+		dice = new DiceCup();
+		// Start the TUI
 		this.tui = new TUI();
 		// Ask for the player names
 		String[] playerNames = tui.gameStart();
-		//Initialize the gui and players
+		// Initialize the gui and players
 		this.gui = new GUIHandler(playerNames);
 		this.players = new Player[playerNames.length];
-		
+
 		for (int i = 0; i < players.length; i++) {
 			this.players[i] = new Player(playerNames[i]);
 		}
-		//Pick a random player to start.
-		turn=(int)(Math.random()*players.length);
+		// Pick a random player to start.
+		turn = (int) (Math.random() * players.length);
 
 	}
-	
-	public void changeTurn(){
-		turn=(turn+1)%players.length;
+
+	public void changeTurn() {
+		turn = (turn + 1) % players.length;
 	}
-	public void movePlayer(){
-		int sum=dice.getDiceValue()[0]+dice.getDiceValue()[1];
-		if(players[turn].getPosition()+sum<=21){
-			players[turn].setPosition(players[turn].getPosition()+sum);
-		}
-		else{
-			int difference=21-players[turn].getPosition();
-			players[turn].setPosition(sum-difference);
+
+	public void movePlayer() {
+		int sum = dice.getDiceValue()[0] + dice.getDiceValue()[1];
+		if (players[turn].getPosition() + sum <= 21) {
+			players[turn].setPosition(players[turn].getPosition() + sum);
+		} else {
+			int difference = 21 - players[turn].getPosition();
+			players[turn].setPosition(sum - difference);
 		}
 	}
-	
-	public void playTurn(){
+
+	public void playTurn() {
 		dice.shakeCup();
 		movePlayer();
 		gui.landOnOwnable(players[turn].getPlayerName(), players[turn].getPosition(), dice.getDiceValue(), "Territory");
 	}
-	
-	
-	public void runGame(){
-		
-	while(true){
-		playTurn();
-		changeTurn();
-	}	
-	
+
+	public void runGame() {
+
+		while (true) {
+			playTurn();
+			changeTurn();
+		}
+
 	}
 
 }
