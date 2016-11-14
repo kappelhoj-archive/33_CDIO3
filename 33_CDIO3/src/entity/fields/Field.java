7c056d8a6d2682043554b22d085d1243e7a2e2a4
Arvid Langsø <arvid.k.langsoe@live.dk>
2016-11-14 15:18:03 +0100

    Moved all the fields to a seperate package

--- 33_CDIO3/src/entity/fields/Field.java
+++ 33_CDIO3/src/entity/fields/Field.java
@@ -1,4 +1,6 @@
-package entity;
+package entity.fields;
+
+import entity.Player;
 
 public abstract class Field 
 {
--- 33_CDIO3/src/entity/fields/Fleet.java
+++ 33_CDIO3/src/entity/fields/Fleet.java
@@ -1,4 +1,4 @@
-package entity;
+package entity.fields;
 
 public class Fleet extends Ownable
 {
