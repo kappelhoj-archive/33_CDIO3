d7d3bb3cf12b62cab3975ed404982f721ebda853
Arvid Langsø <arvid.k.langsoe@live.dk>
2016-11-14 15:47:31 +0100

    Created a game Class

--- 33_CDIO3/src/controller/Game.java
+++ 33_CDIO3/src/controller/Game.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class Game 
-{
+import boundary.GUIHandler;
+import boundary.TUI;
+import entity.Player;
+
+public class Game {
+	Player[] players;
+	GUIHandler gui;
+	TUI tui;
+
+	public Game() {
+
+		this.tui = new TUI();
+		String[] playerNames = tui.gameStart();
+		this.gui = new GUIHandler(playerNames);
+		this.players = new Player[playerNames.length];
+		
+		for (int i = 0; i < players.length; i++) {
+			this.players[i] = new Player(playerNames[i]);
+		}
+
+	}
+
 }
