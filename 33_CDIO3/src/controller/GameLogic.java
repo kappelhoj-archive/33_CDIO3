48f231da555fefc885053f0e62d6a63dd5a12d2e
Mikkel <Mikkel@10.16.230.240>
2016-11-17 10:08:13 +0100

    Added method landOnField in GameLogic

--- 33_CDIO3/src/controller/GameLogic.java
+++ 33_CDIO3/src/controller/GameLogic.java
@@ -4,6 +4,12 @@ import entity.fields.*;
 
 public class GameLogic 
 {
+	/**
+	 * 
+	 * @param player
+	 * @param field
+	 * @param game
+	 */
 	public static void landOnField(Player player, Field field, Game game)
 	{
 		if (field.getType().equals("Labor Camp"))
@@ -13,7 +19,7 @@ public class GameLogic
 		}
 		else if(field.getType().equals("Tax"))
 		{
-			boolean playerPayDecision = game.askForDecision("Tax");
+			boolean playerPayDecision = game.askForDecision("Tax", ((Tax)field).getTaxAmount());
 			player.setPayDecision(playerPayDecision);
 		}
 		field.landOnField(player);
