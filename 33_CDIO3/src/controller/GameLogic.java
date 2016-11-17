d8b20897b6b75613b216acf4055a4d8b090a61db
Mikkel <Mikkel@10.16.230.240>
2016-11-17 13:41:16 +0100

    Added comment.

--- 33_CDIO3/src/controller/GameLogic.java
+++ 33_CDIO3/src/controller/GameLogic.java
@@ -12,11 +12,13 @@ public class GameLogic
 	 */
 	public static String landOnField(Player player, Field field, Game game)
 	{
-		
+		boolean notOwnableFields = field.getType().equals("Refuge") || field.getType().equals("Tax");
 		String msg = "";
-		//Checks if the field is Ownable.
-		if(field.getType().equals("Refuge") || field.getType().equals("Tax") )
+		
+		//Checks if the given field is a Refue/Tax field. (a.k.a. is not a ownable field)
+		if(notOwnableFields)
 		{
+			//If the given field is a tax field, ask the given player if he wants to pay 10% or a specific amount.
 			if (field.getType().equals("Tax"))
 			{
 				boolean playerPayDecision = game.askForDecision("Tax", ((Tax)field).getTaxAmount());
@@ -24,27 +26,35 @@ public class GameLogic
 			}
 			field.landOnField(player);
 		}
+		//Checks if the given field is a Fleet/Territory/LaborCamp field. (a.k.a is a ownable field).
 		else 
 		{
-			if (((Ownable)field).getOwner() == null)
+			//If the field is not owned.
+			if (!((Ownable)field).isFieldOwned())
 			{
-				boolean playerPayDecision = game.askForDecision("buy", 0);
+				//Ask the given player if he wants to buy the field that is not owned.
+				boolean buyField = game.askForDecision("buy", 0);
 				
-				if(playerPayDecision == true)
+				//Checks if the player wants to buy the field.
+				if(buyField)
 				{
+					
 					((Ownable)field).buyField(player);
+					//Check if the player actually bought the field, needed because he might not have money to buy it.
 					if (((Ownable)field).isFieldOwned())
 					{
 						msg = msg + "Bought";
 					}
-					else
-					{
-						msg = msg + "Not bought";
-					}
+				}
+				else
+				{
+					msg = msg + "Not bought";
 				}
 			}
+			//If the field is owned
 			else
 			{
+				//If the given is a Labor camp field. ask for the game for the diceSum to calculate the rent.
 				if(field.getType().equals("Labor Camp"))
 				{
 					int diceSum = game.askForDiceRoll();
@@ -54,6 +64,7 @@ public class GameLogic
 				field.landOnField(player);
 			}
 		}
+		//Checks if the player who had to pay has lost.
 		if (player.getAccountBalance() < 0)
 		{
 			player.setPlayerHasLost(true);
