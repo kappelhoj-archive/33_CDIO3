08a6e79b146ae81d21b5a8205dd11734e8a604ca
Mikkel <Mikkel@10.16.230.240>
2016-11-17 15:51:35 +0100

    Formatting.

--- 33_CDIO3/src/entity/Account.java
+++ 33_CDIO3/src/entity/Account.java
@@ -6,9 +6,7 @@ public class Account {
 
 	/**
 	 * Object Account constructor.
-	 * 
-	 * @param balance
-	 *            The balance which the account should start with.
+	 * @param balance The balance which the account should start with.
 	 */
 	public Account(int balance) {
 		this.balance = balance;
@@ -16,7 +14,6 @@ public class Account {
 
 	/**
 	 * Method getBalance returns the balance of the Account.
-	 * 
 	 * @return balance balance of an account.
 	 */
 	public int getBalance() {
@@ -26,24 +23,26 @@ public class Account {
 	/**
 	 * Method changeBalance calculates the new balance based on the parameter
 	 * value.
-	 * 
-	 * @param value
-	 *            The value that the balance of the account should be changed
-	 *            with. If the parameter is positive the method adds value to
-	 *            the balance. If the parameter is negative the method
-	 *            substracts value from balance.
+	 * @param value The value that the balance of the account should be changed
+	 * with. If the parameter is positive the method adds value to
+	 * the balance. If the parameter is negative the method
+	 * subtracts value from balance.
 	 * @return The method return true if the change of balance succeeded. False
-	 *         otherwise.
+	 * otherwise.
 	 */
-	public void changeBalance(int value) {
+	public void changeBalance(int value) 
+	{
 		// Checks if the balance overflows
-		if (balance + value < balance && value > 0) {
+		if (balance + value < balance && value > 0) 
+		{
 		}
 		// Checks if the balance underflows
-		else if (balance + value > balance && value < 0) {
+		else if (balance + value > balance && value < 0) 
+		{
 		}
 		// Changes balance
-		else {
+		else 
+		{
 			balance = balance + value;
 		}
 
