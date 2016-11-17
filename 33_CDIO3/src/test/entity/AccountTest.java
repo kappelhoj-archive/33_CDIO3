d94813d5325da8cc27dda90065b5229c2fbd198b
Jeppe <keke3k2@gmail.com>
2016-11-17 11:01:37 +0100

    Updated AccountTest with test methods

--- 33_CDIO3/src/test/entity/AccountTest.java
+++ 33_CDIO3/src/test/entity/AccountTest.java
@@ -20,10 +20,56 @@ public class AccountTest {
 	public void tearDown() throws Exception {
 		account = null;
 	}
-
+	
+	/*
+	 * Testing getBalance method
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetBalance() {
+		int expected = 30000;
+		int actual = account.getBalance();
+		assertEquals(expected, actual);
 	}
-
+	
+	/*
+	 * Testing if the changeBalance method subtracts correctly from
+	 * balance when a player has to pay another player.
+	 * In this test case, player1 owns 3 Fleet fields, and player2
+	 * lands on any of player1's Fleet fields and therefor has to pay 2000
+	 * to player1. 
+	 */
+	@Test
+	public void testChangeBalanceSubtraction() {
+		int value = -2000;
+		account.changeBalance(value);
+		
+		int expectedBalance = 28000;
+		int actualBalance = account.getBalance();
+		assertEquals(expectedBalance, actualBalance);
+	}
+	
+	/*
+	 * Testing if the changeBalance method adds correctly to balance when
+	 * a player receives money.
+	 * In this test case, the player lands on "Walled City" field and receives
+	 * 5000 money. 
+	 */
+	@Test
+	public void testChangeBalanceAddition()
+	{
+		int value = 5000;
+		account.changeBalance(value);
+		int expectedBalance = 35000;
+		int actualBalance = account.getBalance();
+		assertEquals(expectedBalance, actualBalance);
+	}
+	
+	/*
+	 * Testing if the changeBalance
+	 */
+	@Test
+	public void testChangeBalanceOverflow()
+	{
+		
+	}
 }
