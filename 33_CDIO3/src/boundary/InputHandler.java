b77b0696ddd1444090737498b668019f70f3e7e3
Arvid Langsø <arvid.k.langsoe@live.dk>
2016-11-20 12:52:33 +0100

    Added 2 methods to the InputHandler

--- 33_CDIO3/src/boundary/InputHandler.java
+++ 33_CDIO3/src/boundary/InputHandler.java
@@ -1,5 +1,13 @@
 package boundary;
 
-public class InputHandler {
+import desktop_resources.GUI;
 
+public class InputHandler {
+		
+		public String AskForString(String msg){
+			return GUI.getUserString(msg);
+		}
+		public int AskForInt(String msg,int min, int max){
+			return GUI.getUserInteger(msg,min,max);
+		}
 }
