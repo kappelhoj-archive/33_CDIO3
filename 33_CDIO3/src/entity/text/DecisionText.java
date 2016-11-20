9fa6d407ad5677bcf8bc23925e835f0e346ffbfc
Arvid Langsø <arvid.k.langsoe@live.dk>
2016-11-20 11:23:00 +0100

    Updated the InputText class so it uses formatting

--- 33_CDIO3/src/entity/text/DecisionText.java
+++ 33_CDIO3/src/entity/text/DecisionText.java
@@ -1,5 +1,26 @@
 package entity.text;
 
-public class DecisionText {
-
+public class DecisionText extends Text{
+	/**
+	 * The method buyfieldText returns a String which asks if you want to buy a field.
+	 * @param playerName The name of the player. 
+	 * @param fieldName of the mattering field.
+	 * @param price(int) of the mattering field.
+	 * @return String
+	 */
+	public static String buyfieldText(String playerName, String fieldName, String price)
+	{
+		return "Do you, " + playerName + ", want to buy "+ fieldName +" for "+ price +"? \n";
+	}
+	/**
+	 * The method taxText returns a String which tells you, landed on a tax field.
+	 * @param plyerName The name of the player.
+	 * @param fieldName The name of the field.
+	 * @param tax The tax of the field.
+	 * @return String
+	 */
+	public static String taxText(String playerName, String fieldName, int tax)
+	{
+		return playerName +" landed on " + fieldName + " you will have to pay tax, you can choose to pay "+ tax +" or pay 10% of your fortune \n";
+	}
 }
--- 33_CDIO3/src/entity/text/InputText.java
+++ 33_CDIO3/src/entity/text/InputText.java
@@ -1,5 +1,39 @@
 package entity.text;
 
-public class InputText {
+public class InputText extends Text {
+	/**
+	 * Argument 0 is a welcome text. Argument 1 ask for the number players that
+	 * are going to play Argument 2 ask for name of player number x. Needs:
+	 * (int) Argument 3 says the rules of the game Argument 4 States that a
+	 * random player has been chosen to start.
+	 */
+	public static String[] information = { "Welcome to 'Rodatam' \n",
+			"Enter the number of players who wants to play.\n", "Please enter the name of player %d \n",
+			gameRulesText(), "A player has been randomly selected to start the game. \n" };
+
+	/**
+	 * Error 0, invalid number of players.
+	 * Error 1, invalid player name.
+	 */
+	public static String[] errors = {
+			"This is not a valid number of players, please try again with a number within 2-6 \n",
+			"This player name %s is already taken, try again \n" };
+
+	/**
+	 * The method gameRules returns a string which contains the game rules.
+	 * 
+	 * @return String
+	 */
+	public static String gameRulesText() {
+		String gameRules = "";
+		gameRules = "The game rules are as follows: \n";
+		gameRules = gameRules + "- All players start with 30000 money. \n"
+				+ "- When you lose all your money you are out of the game. \n"
+				+ "- The last remaining player has won the game.\n"
+				+ "- You start a turn by rolling the dice. You then move forward by the amount you rolled.\n"
+				+ "- There are 5 different types of fields.\n" + "- ";
+
+		return gameRules;
+	}
 
 }
