a3b7c459752f415a8cb459cfb776046fce9c199c
Mikkel <Mikkel@mikkels-mbp-2.eduroam.wireless.dtu.dk>
2016-11-23 13:41:02 +0100

    Updated FleetTest.

--- 33_CDIO3/src/test/entity/fields/FleetTest.java
+++ 33_CDIO3/src/test/entity/fields/FleetTest.java
@@ -10,71 +10,86 @@ import entity.Player;
 import entity.fields.Fleet;
 
 public class FleetTest {
-	Fleet fleetField1, fleetField2, fleetField3, fleetField4;
+	Fleet fleet1, fleet2, fleet3, fleet4;
 	Player player1, player2;
 
 	@Before
-	public void setUp() throws Exception {
-		fleetField1 = new Fleet("Fleet", 4000);
-		fleetField2 = new Fleet("Fleet", 4000);
-		fleetField3 = new Fleet("Fleet", 4000);
-		fleetField4 = new Fleet("Fleet", 4000);
+	public void setUp() throws Exception 
+	{
+		//Creates four fleet fields and two players.
+		fleet1 = new Fleet("Fleet", 4000);
+		fleet2 = new Fleet("Fleet", 4000);
+		fleet3 = new Fleet("Fleet", 4000);
+		fleet4 = new Fleet("Fleet", 4000);
 		player1 = new Player("Morten");
 		player2 = new Player("Peter");
 	}
 
 	@After
-	public void tearDown() throws Exception {
-		fleetField1 = null;
-		fleetField2 = null;
-		fleetField3 = null;
-		fleetField4 = null;
+	public void tearDown() throws Exception 
+	{
+		//Resets the objects.
+		fleet1 = null;
+		fleet2 = null;
+		fleet3 = null;
+		fleet4 = null;
 		player1 = null;
 		player2 = null;
 	}
 	
-	/*
-	 * Testing all Rent values for how many Fleet fields are owned.
+	/**
+	 * Method testGetRent tests if the getRent method returns the correct 
+	 * rent based on the amount of fleets owned by the player.
 	 */
 	@Test
-	//Test to see if amount of Fleets owned by a player is correctly added up, so the rent on each Fleet-field rises.
-	public void testGetRent() {
-		// Array with all the Fleet fields
-		Fleet[] fleetFields = {fleetField1, fleetField2, fleetField3, fleetField4};
-		// Array with the different Fleet rent values
+	public void testGetRent() 
+	{
+		// Instantiates a fleet[] containing all the fleets.
+		Fleet[] fleetFields = {fleet1, fleet2, fleet3, fleet4};
+		// Instantiates a int[] containing the expected rents when 1,2,3 and 4 fleets are owned.
 		int[] expectedRents = {500, 1000, 2000, 4000};
 		
+		// This for-loop buys 1 fleet at a time and checks if the rent is correct when landed on.
 		// Buy 1 to 4 Fleet fields and check if the Rent values are correct.
 		for(int i = 0; i < fleetFields.length; i++)
 		{
+			// player1 buys one fleet per iteration.
 			fleetFields[i].buyField(player1);
-			
+			// The expected rent with i fleet fields.
 			int expectedRent = expectedRents[i];
-			int actualRent = fleetField1.getRent();
-			assertEquals("Owned Fleet fields: " + (i+1) + " Rent: " + expectedRents[i], expectedRent, actualRent);
-			//System.out.println("Owned Fleet fields: " + (i+1) + " Rent: " + expectedRents[i]);
+			// The actual rent with i fleet fields.
+			int actualRent = fleet1.getRent();
+			assertEquals("Amount of owned fleet fields are " + (i+1) + " The expected rent is: " + expectedRents[i] + " but was " + actualRent, expectedRent, actualRent);
 		}
 	}
 	
-	/*
-	 * Testing if landOnField for the Fleet field works as intended.
+	/**
+	 * Method testLandOnFieldFleet tests if the correct things happens when a player lands on a fleet field.
 	 */
 	@Test
-	public void testLandOnFieldFleet() {
-		fleetField1.buyField(player1);
-		fleetField2.buyField(player1);
+	public void testLandOnFieldFleet() 
+	{
+		// player1 buys fleet1 and fleet2.
+		fleet1.buyField(player1);
+		fleet2.buyField(player1);
+		// player2 lands on fleet2
+		fleet2.landOnField(player2);
 		
-		fleetField2.landOnField(player2);
+		//Checks if the balance of player1 is updated correctly.
 		
-		// The expected player1 balance is 30000 minus the two Fleet fields
-		// he bought (2*4000) plus the 1000 rent price from player2.
+		// The expected balance of player1 is 30000 minus the two Fleet fields that he bought (2*4000) 
+		// plus the 1000 rent from player2.
 		int expectedPlayer1Balance = 30000 - (2*4000) + 1000;
+		//The actual balance of player1
 		int actualPlayer1Balance = player1.getAccountBalance();
-		assertEquals(expectedPlayer1Balance, actualPlayer1Balance);
+		assertEquals("The balance of player1 was expected to be " + expectedPlayer1Balance +  " but was " + actualPlayer1Balance,expectedPlayer1Balance, actualPlayer1Balance);
 		
-		// The expected player2 balance is 30000 minus the 1000 rent price.
+		//Checks if the balance of player2 is updated correctly.
+		
+		// The expected balance of player2 is 30000 minus the 1000 rent price.
 		int expectedPlayer2Balance = 30000 - 1000;
+		//The actual balance of player2
 		int actualPlayer2Balance = player2.getAccountBalance();
-		assertEquals(expectedPlayer2Balance, actualPlayer2Balance);
+		assertEquals("The balance of player 2 was expected to be " + expectedPlayer2Balance + " but was " + actualPlayer2Balance,expectedPlayer2Balance, actualPlayer2Balance);
 	}
 }
